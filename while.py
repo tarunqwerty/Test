3a3494b7317c41800c30445ca7e67cfc4faf50cc
Tarun Yadav <tarun.yadav@leena.ai>
2024-05-21 13:17:37 +0530

    first commit

---
+++ while.py
@@ -0,0 +1,20 @@
+
+""" a=1
+while(a<=4):
+    print("bye")
+    a=a+1 """
+   
+
+b=2
+c=1
+print(b)
+""" 
+while(c<=10):
+    print(f'{b} * {c} = {b*c}')
+    c=c+1 """
+
+d=8000
+
+for i in range(1,15):
+    d=d*2
+    print(f'{i} year {d}')
